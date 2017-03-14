de8b34e9a7ef8bf15e91511c54e87b5d07789029
俞彬 <303734023@qq.com>
2017-03-14 14:43:28 +0800

    第二章练习

---
+++ src/com/chapterTwo/exercise/Answer0201.scala
@@ -0,0 +1,52 @@
+package com.chapterTwo.exercise
+
+import java.beans.Transient
+
+import scala.annotation.tailrec
+
+/**
+  *
+  * 题目:写一个递归函数，来获取第N个斐波那契前两个斐波那契数0和1，第N个数总是等于它前两个数的和——序列开始为0、1、1、2、3、5。
+  * 应该定义为局部（local）尾递归函数
+  * Created by IntelliJ IDEA.
+  * User: bin.yu
+  * Date: 2017/3/14 
+  * Time: 14:10
+  *
+  */
+
+object Answer0201 {
+
+  /**
+    *
+    * 递归解法
+    *
+    * @param n 第几个斐波那契数
+    * @return 斐波那契数
+    */
+  def fibNotTailrec(n: Int): Int = {
+    if (n <= 2) {
+      n - 1
+    } else {
+      fib(n - 1) + fib(n - 2)
+    }
+  }
+
+  /**
+    * 尾递归解法
+    *
+    * @param n
+    * @return
+    */
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def loop(n: Int, prev: Int, cur: Int): Int =
+      if (n == 0) prev
+      else loop(n - 1, cur, prev + cur)
+    loop(n - 1, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(fib(6))
+  }
+}
