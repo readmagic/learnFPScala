df4177910a05e6abf6368cedb97242d25c99e9b9
frandy <303734023@qq.com>
2022-01-20 23:08:43 +0800

    修改代码

--- build.sbt
+++ build.sbt
@@ -2,4 +2,4 @@ name := "learnFPScala"
 
 version := "0.1"
 
-scalaVersion := "2.12.8"
+scalaVersion := "2.12.2"
--- src/main/scala/ch1/course/Course1.scala
+++ src/main/scala/ch1/course/Course1.scala
@@ -7,7 +7,7 @@ class Coffee() {val price = 100}
 class Cafe {
   def buyCoffee(cc: CreditCard): Coffee = {
     val cup = new Coffee()
-    cc.charge(cup.price)//副作用房产vbvv
+    cc.charge(cup.price)//副作用
     cup
   }
 }
